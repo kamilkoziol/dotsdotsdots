2ad4e33c600ed1a8749695e666b08f3fe5861f1d
Kamil Koziol <kamkoziol1@gmail.com>
2026-05-05 21:15:44 +0200

    treesitter branch change

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,8 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  branch = 'main',
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
